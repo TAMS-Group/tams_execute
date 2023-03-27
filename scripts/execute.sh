2850614b26e45e90742cf43d04ecf4f24cb0540d
v4hn <me@v4hn.de>
2023-03-27 08:24:01 +0000

    fix previously-untested edge-cases
    eval allows more flexible specifications of the workspace location.
    (At the cost of an overly generic interface).

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# v4hn@20230324
 
-# this script optionally sources a separate workspace and executes something in that context
+# this script optionally sources a separate workspace
+# and executes a roslaunch file or executable in that context
 
-if [[ $# -lt 3 ]]; then
+if [[ $# -lt 2 ]]; then
   echo "usage: $0 <pkg> <executable-or-launch-file> [<workspace>]" >&2
   exit 1
 fi
@@ -12,7 +14,8 @@ exe="$2"
 ws="$3"
 
 if [[ -n "$ws" ]]; then
-  . $ws/setup.bash
+  # use eval to support '~' and environment variables
+  eval source $ws/setup.bash
 fi
 
 if [[ "$2" = *.launch ]]; then
